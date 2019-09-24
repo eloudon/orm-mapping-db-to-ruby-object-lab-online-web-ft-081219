275841f43f4b64b8fb7c81e8d12bcd3c6fba7279
Eric Loudon <loudon.eric@gmail.com>
2019-09-24 11:25:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,3 +64,15 @@ class Student
     DB[:conn].execute(sql)
   end
 end
+
+def self.count_all_students_in_grade_9
+   sql = <<-SQL
+     SELECT COUNT(*)
+     FROM students
+     WHERE grade = 9
+   SQL
+
+   DB[:conn].execute(sql).map do |row|
+     self.new_from_db(row)
+   end
+ end
