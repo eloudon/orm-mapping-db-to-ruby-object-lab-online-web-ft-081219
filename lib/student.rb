80d8b7a50f4d504c6e74750ae6edbd24a18abf2a
Eric Loudon <loudon.eric@gmail.com>
2019-09-24 11:28:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,4 +63,58 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = "9"
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < "12"
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.first_x_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = "10"
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql,x)
+  end
+
+  def self.first_student_in_grade_10
+    # sql = <<-SQL
+    #   SELECT *
+    #   FROM students
+    #   WHERE grade = "10"
+    #   LIMIT 1
+    # SQL
+
+    # x = DB[:conn].execute(sql)
+    x = self.first_x_students_in_grade_10(1)
+    self.new_from_db(x.first)
+  end
+
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql,x)
+  end
 end
